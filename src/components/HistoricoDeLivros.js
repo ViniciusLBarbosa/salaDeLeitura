757a624204eccca07847eaa7552b8a8fde21ed72
Vinícius de Lima Barbosa <159471546+ViniciusLBarbosa@users.noreply.github.com>
2024-12-02 16:57:49 -0300

    fix

--- src/components/HistoricoDeLivros.js
+++ src/components/HistoricoDeLivros.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebase';
 import '../styles.css';
 import './tabela.css'
